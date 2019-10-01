f1681ea9ed6d0324458fd86c660a3948ad4b36c6
Alexey Cherevko <you@example.com>
2019-10-01 17:18:45 +0700

    17-15 0110

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,15 +3,15 @@
 namespace app\controllers;
 
 use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\web\Response;
-use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
+use app\models\Signup;
 
 class SiteController extends Controller{
     public function actionIndex(){
         return $this->render('index');
+    }
+    public function actionSignup(){
+        $model=new Signup();
+        return $this->render('signup', ['model'=>$model]);
     }
 }
